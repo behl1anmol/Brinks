ecce36b75b1d118acc5bb5e3c220ca263d579128
behl1anmol <behl1anmol@gmail.com>
2018-03-17 23:34:19 +0530

    brinks

--- Code/main.cpp
+++ Code/main.cpp
@@ -17,6 +17,15 @@
 #define clrscr()system("cls");
 #define MAX_NUM 10;
 using namespace std;
+
+char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+int checkwin();
+void board();
+void tictactoe();
+
+
+
+
 Learner AIG;
 int i=0,j,cs,k=1;
 string identity;
@@ -998,6 +1007,7 @@ void game()
    AIg.respond1("let's play !");
    AIg.respond1("PRESS 1 TO PLAY QUIZ");
    AIg.respond1("PRESS 2 TO PLAY SNAKE");
+   AIg.respond1("PRESS 3 TO PLAY TIC TAC TOE");
    cout<<"YOU:";
    cin>>choice;
    //cs=snake1();
@@ -1012,6 +1022,10 @@ void game()
     break;
    case 2:
     cs=snake1();
+    break;
+   case 3:
+    tictactoe();
+    clrscr();
     break;
    default:
     break;
@@ -1080,3 +1094,106 @@ void kiet()
  getch();
  system("COLOR F0");
 }
+
+void tictactoe()
+{
+	int player = 1,i,choice;
+	char mark;
+	do
+	{
+		board();
+		player=(player%2)?1:2;
+		cout << "Player " << player << ", enter a number:  ";
+		cin >> choice;
+		mark=(player == 1) ? 'X' : 'O';
+		if (choice == 1 && square[1] == '1')
+			square[1] = mark;
+		else if (choice == 2 && square[2] == '2')
+			square[2] = mark;
+		else if (choice == 3 && square[3] == '3')
+			square[3] = mark;
+		else if (choice == 4 && square[4] == '4')
+			square[4] = mark;
+		else if (choice == 5 && square[5] == '5')
+			square[5] = mark;
+		else if (choice == 6 && square[6] == '6')
+			square[6] = mark;
+		else if (choice == 7 && square[7] == '7')
+			square[7] = mark;
+		else if (choice == 8 && square[8] == '8')
+			square[8] = mark;
+		else if (choice == 9 && square[9] == '9')
+			square[9] = mark;
+		else
+		{
+			cout<<"Invalid move ";
+			player--;
+			cin.ignore();
+			cin.get();
+		}
+		i=checkwin();
+		player++;
+	}while(i==-1);
+	board();
+	if(i==1)
+		cout<<"==>\aPlayer "<<--player<<" win ";
+	else
+		cout<<"==>\aGame draw";
+	cin.ignore();
+	cin.get();
+	//return 0;
+}
+/*********************************************
+	FUNCTION TO RETURN GAME STATUS
+	1 FOR GAME IS OVER WITH RESULT
+	-1 FOR GAME IS IN PROGRESS
+	O GAME IS OVER AND NO RESULT
+**********************************************/
+
+int checkwin()
+{
+	if (square[1] == square[2] && square[2] == square[3])
+		return 1;
+	else if (square[4] == square[5] && square[5] == square[6])
+		return 1;
+	else if (square[7] == square[8] && square[8] == square[9])
+		return 1;
+	else if (square[1] == square[4] && square[4] == square[7])
+		return 1;
+	else if (square[2] == square[5] && square[5] == square[8])
+		return 1;
+	else if (square[3] == square[6] && square[6] == square[9])
+		return 1;
+	else if (square[1] == square[5] && square[5] == square[9])
+		return 1;
+	else if (square[3] == square[5] && square[5] == square[7])
+		return 1;
+	else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+		return 0;
+	else
+		return -1;
+}
+
+
+/*******************************************************************
+     FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+********************************************************************/
+
+
+void board()
+{
+	system("cls");
+	cout << "\n\n\tTic Tac Toe\n\n";
+	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+	cout << "     |     |     " << endl << endl;
+}
+
